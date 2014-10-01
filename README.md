63645de3bfec69dac06ea953026842bd60b676b7
Josh Simmons <josh.simmons@gmail.com>
2014-09-30 20:51:50 -0700

    Adding license, readme, and splitting out library.

---
+++ README.md
@@ -0,0 +1,11 @@
+animalese.js
+================
+
+animalese.js is a JavaScript library that synthesizes an approximation of the
+villager speech sounds from Animal Crossing.
+
+It uses [riffwave.js](http://www.codebase.es/riffwave/) to turn audio data into
+a DataURI, which can then be played back through an <audio> tag. Also makes use
+of [FileSaver.js](https://github.com/eligrey/FileSaver.js) and
+[Blob.js](https://github.com/eligrey/Blob.js). Thanks to Pedro Ladaria and Eli
+Grey for these excellent tools :)
---
+++ animalese.js
@@ -0,0 +1,51 @@
+// animalese.js
+// (C) 2014 Josh Simmons
+// http://github.com/acedio/animalese.js
+
+var Animalese = function(letters_file, onload) {
+    this.Animalese = function(script) {
+        var data = [];
+
+        var sample_freq = 44100;
+        var channels = 1;
+        var letter_secs = 0.15;
+        var letter_spoken_secs = 0.075;
+        var letter_size = Math.floor(letter_secs * channels * sample_freq);
+        var letter_spoken_size =
+            Math.floor(letter_spoken_secs * channels * sample_freq);
+
+        for (var c_index = 0; c_index < script.length; c_index++) {
+            var c_value = script.toUpperCase()[c_index];
+            if (c_value >= 'A' && c_value <= 'Z') {
+                var letter_loc =
+                    letter_size * (c_value.charCodeAt(0) - 'A'.charCodeAt(0));
+
+                for (var i = 0; i < letter_spoken_size; i++) {
+                    data[c_index*letter_spoken_size + i] =
+                        this.letters[44 + letter_loc + i];
+                }
+            } else { // non pronouncable character or space
+                for (var i = 0; i < letter_spoken_size; i++) {
+                    data[c_index*letter_spoken_size + i] = 127;
+                }
+            }
+        }
+
+        var wave = new RIFFWAVE();
+        wave.header.sampleRate = sample_freq;
+        wave.header.numChannels = channels;
+        wave.Make(data);
+
+        return wave;
+    }
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', letters_file);
+    xhr.responseType = 'arraybuffer';
+    var req = this;
+    xhr.onload = function(e) {
+        req.letters = new Uint8Array(this.response);
+        onload();
+    }
+    xhr.send();
+}
